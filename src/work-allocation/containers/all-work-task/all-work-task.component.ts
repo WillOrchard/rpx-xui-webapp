886e3616528965a8ce520af2512d9000732bd168
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2023-04-11 11:33:25 +0100

    lint error

--- src/work-allocation/containers/all-work-task/all-work-task.component.ts
+++ src/work-allocation/containers/all-work-task/all-work-task.component.ts
@@ -73,7 +73,7 @@ export class AllWorkTaskComponent extends TaskListWrapperComponent {
     ).pipe(map(jurisdictions => {
       this.supportedJurisdictions = jurisdictions[1];
       const result = jurisdictions[0].includes(null) ? jurisdictions[1] : jurisdictions[0];
-      return [...new Set(result)]
+      return [...new Set(result)];
     }));
   }
 
